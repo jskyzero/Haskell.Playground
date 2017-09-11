44ff8586b55bd569f47ca8e6ddcb0eec522a30e1
jskyzero <jskyzero@outlook.com>
2017-09-11 12:00:11 +0800

    add function example

---
+++ hardwork/function.hs
@@ -0,0 +1,13 @@
+-- some code pratice haskell function
+-- how to run it ?
+--    use ghci and load this file, then try use this function
+--    if you don't konw how to use ghci or load it, see the docs/ghci-begin.md
+
+-- test example
+-- :load function.hs
+-- double 2
+-- multi_two 2 2
+
+double x = x * 2
+square x = x ^ 2
+multi_two x y = x * y
